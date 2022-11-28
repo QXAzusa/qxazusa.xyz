aaf10293d3412e83f130b08f1dde1feaa99d0451
浅笑Azusa <69675439+QXAzusa@users.noreply.github.com>
2022-11-28 11:03:40 +0800

    Update Close

--- src/pages/error-page/Close.js
+++ src/pages/error-page/Close.js
@@ -1,14 +1,7 @@
-import React from 'react';
-import Layout from '@theme/Layout';
-import Head from '@docusaurus/Head';
+import React from 'react'
+import ReactDOM from 'react-dom'
 
-export default function Hello() {
-  return (
-    <Layout title="网站维护中，请稍后访问" description="">
-    <head><meta name="robots" content="nOiNdeX, NoFolLoW" /></head>
-    <main>
-    网站维护中，请稍后访问
-    </main>
-    </Layout>
-  );
-}
+// 创建元素
+const title= <div><h3>网站维护中，请稍后访问!</h3><a target="_blank" rel="noopener" href="https://www.upyun.com/?utm_source=lianmeng&amp;utm_medium=referral" style="text-decoration: none;color:var(--ifm-footer-link-color)!important">本站由&nbsp;<img id="upyun_logo" alt="upyun_logo" src="//static.qxazusa.xyz/public/image/upyun_logo.png" align="center">&nbsp;提供CDN加速服务</a><br><a href="https://beian.miit.gov.cn/" target="_blank" rel="noopener" style="text-decoration: none;color:var(--ifm-footer-link-color)!important">苏ICP备2021044611号-1</a>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<a href="http://www.beian.gov.cn/portal/registerSystemInfo?recordcode=32081202000264" target="_blank" rel="noopener" style="text-decoration: none;color:var(--ifm-footer-link-color)!important">苏公网安备 32081202000264号</a>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<a href="https://icp.gov.moe/?keyword=20220417" target="_blank" style="text-decoration: none;color:var(--ifm-footer-link-color)!important">萌ICP备20220417号</a></div>
+
+ReactDOM.render(title, document.getElementById('root'))
