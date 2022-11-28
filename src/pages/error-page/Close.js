b7d9643add5ab80b91434076eb4bf82299331388
浅笑Azusa <69675439+QXAzusa@users.noreply.github.com>
2022-11-28 10:49:14 +0800

    Add Close Page

---
+++ src/pages/error-page/Close.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Layout from '@theme/Layout';
+import Head from '@docusaurus/Head';
+
+export default function Hello() {
+  return (
+    <Layout title="网站维护中，请稍后访问" description="">
+    <head><meta name="robots" content="nOiNdeX, NoFolLoW" /></head>
+    <main>
+    网站维护中，请稍后访问
+    </main>
+    </Layout>
+  );
+}
