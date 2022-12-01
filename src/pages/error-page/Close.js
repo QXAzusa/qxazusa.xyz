03f130738ac9255a645a69ec4ddf15f5f576a6f7
浅笑Azusa <69675439+QXAzusa@users.noreply.github.com>
2022-12-01 14:33:40 +0800

    Delete Close.js

--- src/pages/error-page/Close.js
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-
-// 创建元素
-const title= <div><h3>网站维护中，请稍后访问!</h3><a href="https://beian.miit.gov.cn/" target="_blank" rel="noopener" style="text-decoration: none;color:var(--ifm-footer-link-color)!important">苏ICP备2021044611号-1</a>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<a href="http://www.beian.gov.cn/portal/registerSystemInfo?recordcode=32081202000264" target="_blank" rel="noopener" style="text-decoration: none;color:var(--ifm-footer-link-color)!important">苏公网安备 32081202000264号</a>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<a href="https://icp.gov.moe/?keyword=20220417" target="_blank" style="text-decoration: none;color:var(--ifm-footer-link-color)!important">萌ICP备20220417号</a></div>
-
-ReactDOM.render(title, document.getElementById('root'))
