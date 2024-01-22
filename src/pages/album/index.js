93d6a048245af8e0bd11fab1bd7083e5bf0062e8
QXAzusa <15129182-QXAzusa@users.noreply.gitlab.com>
2024-01-22 14:18:51 +0800

    Add More zh-Hant Translates

--- src/pages/album/index.js
+++ src/pages/album/index.js
@@ -5,12 +5,12 @@ import Translate, {translate} from '@docusaurus/Translate';
 
 export default function Hello() {
     return (
-        <Layout title="Album" description="Album Page">
+        <Layout title={translate({message: 'Album'})} description="Album Page">
             <head><meta name="robots" content="noindex, nofollow" /></head>
             <main class="container margin-vert--xl">
                 <div class="row">
                     <div class="col col--6 col--offset-3">
-                        <h1 class="hero__title">Album</h1>
+                        <h1 class="hero__title">{translate({message: 'Album'})}</h1>
                         <h1 class="hero__subtitle">To be continued...</h1>
                     </div>
                 </div>
--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,22 +1,23 @@
 import React from 'react';
 import Layout from '@theme/Layout';
 import Head from '@docusaurus/Head';
+import Translate, {translate} from '@docusaurus/Translate';
 
 export default function Hello() {
   return (
-    <Layout title="联系方式" description="Contact Page">
+    <Layout title={translate({message: '联系方式'})} description="Contact Page">
     <head><meta name="robots" content="noindex, nofollow" /></head>
     <main class="container margin-vert--xl">
         <div class="row">
             <div class="col col--6 col--offset-3">
-                <h1 class="hero__title">联系方式</h1>
-                <h1 class="hero__subtitle">哔哩哔哩</h1>
-                <p><a href='https://space.bilibili.com/148833361'>主页</a></p>
-                <p><a href='https://message.bilibili.com/#whisper/mid148833361'>私信（需要登陆）</a></p>
-                <h1 class="hero__subtitle">棉花糖</h1>
+                <h1 class="hero__title"><Translate>联系方式</Translate></h1>
+                <h1 class="hero__subtitle"><Translate>哔哩哔哩</Translate></h1>
+                <p><a href='https://space.bilibili.com/148833361'><Translate>主页</Translate></a></p>
+                <p><a href='https://message.bilibili.com/#whisper/mid148833361'><Translate>私信（需要登陆）</Translate></a></p>
+                <h1 class="hero__subtitle"><Translate>棉花糖</Translate></h1>
                 <p><a href='https://marshmallow-qa.com/qxazusa'>marshmallow-qa.com/qxazusa</a></p>
-                <h1 class="hero__subtitle">电子邮箱</h1>
-                <p>webmaster#qxazusa.xyz（#改为@）</p>
+                <h1 class="hero__subtitle"><Translate>电子邮箱</Translate></h1>
+                <p>webmaster#qxazusa.xyz</p>
             </div>
         </div>
     </main>
