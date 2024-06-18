26bbf5834171b191286111225f114fc80d7e63d3
QXAzusa <15129182-QXAzusa@users.noreply.gitlab.com>
2024-06-19 00:00:00 +0800

    Add Album Demo

--- src/pages/album/index.js
+++ src/pages/album/index.js
@@ -1,20 +1,50 @@
 import React from 'react';
 import Layout from '@theme/Layout';
-import Head from '@docusaurus/Head';
-import Translate, {translate} from '@docusaurus/Translate';
+import { VerticalTimeline, VerticalTimelineElement } from 'react-vertical-timeline-component';
+import 'react-vertical-timeline-component/style.min.css';
+import './styles.css'
+const PhotoAlbum = () => {
+    const photos = [
+        {
+          year: 2024,
+          images: [
+            { src: 'https://static.qxazusa.xyz/blog/album/cos/IMG_20220223_161738~2.png', description: '图片1的说明' }
+          ]
+        },
+        {
+          year: 2023,
+          images: [
+            { src: 'https://static.qxazusa.xyz/blog/album/cos/20220223163708.jpg', description: '图片2的说明' }
+          ]
+        },
+        // 添加更多年份和图片
+      ];
 
-export default function Hello() {
-    return (
-        <Layout title={translate({message: 'Album'})} description="Album Page">
-            <head><meta name="robots" content="noindex, nofollow" /></head>
-            <main class="container margin-vert--xl">
-                <div class="row">
-                    <div class="col col--6 col--offset-3">
-                        <h1 class="hero__title">{translate({message: 'Album'})}</h1>
-                        <h1 class="hero__subtitle">To be continued...</h1>
-                    </div>
+  return (
+    <Layout title="相册" description="这是一个相册页面">
+      <div style={{ padding: '2rem' }}>
+        <h1>Demo</h1>
+        <VerticalTimeline>
+          {photos.map((photo, index) => (
+            <VerticalTimelineElement
+            key={index}
+            date={photo.year.toString()}
+            iconStyle={{ background: 'rgb(33, 150, 243)', color: '#fff', transform: 'scale(0.6)' }} // 缩小节点大小
+            contentStyle={{ textAlign: 'left' }} // 左对齐内容
+            icon={<div className="custom-icon"></div>} // 使用自定义图标
+          >
+              {photo.images.map((image, idx) => (
+                <div key={idx} style={{ marginBottom: '10px' }}>
+                  <img src={image.src} alt={`图片${idx + 1}`} style={{ width: '100%' }} />
+                  <p>{image.description}</p>
                 </div>
-            </main>
-        </Layout>
-    );
-}
+              ))}
+            </VerticalTimelineElement>
+          ))}
+        </VerticalTimeline>
+      </div>
+    </Layout>
+  );
+};
+
+export default PhotoAlbum;
--- src/pages/album/index.md.bak
+++
@@ -1,7 +0,0 @@
----
-title: 相册
-date: 2021-08-02 16:13:41
----
-# 相册
-## [Cosplay](/album/cosplay)
-## [Wallpaper](/album/wallpaper)
